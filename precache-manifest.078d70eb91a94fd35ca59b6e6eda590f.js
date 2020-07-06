d218f632712d7746f8a108cc3b965c580cb69274
evgenborod496 <eugeneborodko@gmail.com>
2020-07-06 11:36:05 +0300

    Updates

---
+++ precache-manifest.078d70eb91a94fd35ca59b6e6eda590f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e3d407e0ed3a03989b57a9838eac4b",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "d5ad53af6893675442c3",
+    "url": "/to-do-list/static/css/main.44f72529.chunk.css"
+  },
+  {
+    "revision": "ba38f803f35005266e3f",
+    "url": "/to-do-list/static/js/2.18e65408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.18e65408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ad53af6893675442c3",
+    "url": "/to-do-list/static/js/main.881af899.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
---
+++ static/js/main.881af899.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpto-do-list"]=this["webpackJsonpto-do-list"]||[]).push([[0],{3:function(t,n,e){t.exports=e(9)},8:function(t,n,e){},9:function(t,n,e){"use strict";e.r(n);var o=e(0),r=e.n(o),a=e(2),c=e.n(a),i=function(){return r.a.createElement("div",{className:"container body__container"},r.a.createElement("div",null,"Entry point..."))};e(8);c.a.render(r.a.createElement(i,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.881af899.chunk.js.map
